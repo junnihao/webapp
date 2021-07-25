f31247981c09088401e0d8536a64187f332e2760
junnihao <461425375@qq.com>
2021-07-25 14:09:20 +0800

    20210725-1

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { ElMessageBox } from 'element-plus'
-import router from '@/router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import router from '@/router'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8080/'
 axios.defaults.withCredentials = true
@@ -10,14 +10,25 @@ axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers['token'] = localStorage.getItem('token') || ''
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-NProgress.inc(0.2)
-NProgress.configure({ easing: 'ease', speed: 600, showSpinner: false })
+// NProgress.inc(1.2)
+NProgress.configure({ easing: 'ease', speed: 6000, showSpinner: false })
+
 
 /*axios.inCharClass.request.use(function (config) {
     // 启动进度条
     NProgress.start()
     return config
 })*/
+
+axios.interceptors.request.use(function (config) {
+  // 出现进度条
+  NProgress.start()
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
 
 axios.interceptors.response.use(res => {
     if (typeof res.data !== 'object') {
